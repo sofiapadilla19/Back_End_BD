d1fd4dce318dec65b20adbc92f9335f00fffc05e
sofiapadilla19 <66934105+sofiapadilla19@users.noreply.github.com>
2022-11-11 05:12:38 -0600

    primmer-commit

---
+++ Back_End_BD/Controllers/AlumnosController.cs
@@ -0,0 +1,74 @@
+﻿using Back_End_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Back_End_BD.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            using(AlumnoDbContext dbAlumno= new AlumnoDbContext())
+            {
+                return View(dbAlumno.Alumnos.ToList());
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            using (AlumnoDbContext dbAlumno = new AlumnoDbContext())
+            {
+                Alumnos alumno = dbAlumno.Alumnos.Find(id);
+                if(alumno == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(alumno);
+            }
+        }
+        [HttpPost]
+
+        public ActionResult Create(Alumnos alum)
+        {
+            using (AlumnoDbContext dbAlumno = new AlumnoDbContext())
+            {
+                dbAlumno.Alumnos.Add(alum);
+                dbAlumno.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+       
+        public ActionResult Edit(int? id)
+        {
+            using (AlumnoDbContext dbAlumno = new AlumnoDbContext())
+            {
+                return View(dbAlumno.Alumnos.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Alumnos alum)
+        {
+            using (AlumnoDbContext dbAlumno = new AlumnoDbContext())
+            {
+                dbAlumno.Entry(alum).State = System.Data.Entity.EntityState.Modified;
+                dbAlumno.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Alumnos alum, int? id)
+        {
+            using (AlumnoDbContext dbAlumno = new AlumnoDbContext())
+            {
+                Alumnos al = dbAlumno.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+                dbAlumno.Alumnos.Remove(al);
+                dbAlumno.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Back_End_BD/Models/AlumnoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Back_End_BD.Models
+{
+    public class AlumnoDbContext:DbContext
+    {
+        private static string connection1= "DefaultConnection";
+        public AlumnoDbContext():base(connection1)
+        {
+
+        }
+        public DbSet<Alumnos> Alumnos { get; set; }
+        public DbSet<Maestros> Maestros { get; set; }
+    }
+}
